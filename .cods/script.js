a8c254fd4f2711c9ce49a36cca94764456969e6f
Rayane Pereira <raypereirasilva13@gmail.com>
2022-01-15 17:30:43 -0300

    Update script.js

---
+++ .cods/script.js
@@ -0,0 +1,31 @@
+let order = []
+let clickedOrder = []
+let scores = 0;
+
+//0 - blue
+//1 - yellow
+//2 - red
+//3 - green
+//4 - pink
+//5 - orange
+//6 - purple
+//7 - aquamarine
+
+const blue = document.querySelector('.blue');
+const yellow = document.querySelector('.yellow');
+const red = document.querySelector('.red');
+const green = document.querySelector('.green');
+const pink = document.querySelector('.pink');
+const orange = document.querySelector('.orange');
+const purple = document.querySelector('.purple');
+const aquamarine = document.querySelector('.aquamarine');
+
+let shuffleOder = () => {
+    let colorOrder = Math.floor(Math.random() *8);
+    order[order.length] = colorOrder;
+    clickedOrder = [];
+    
+    for(let i in order) {
+        let elementColor =
+    }
+}
